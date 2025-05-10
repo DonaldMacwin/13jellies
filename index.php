22070065514779f1dfc1dc87ed4dcc85b3ce2814
Wakabayashi <donald_macwin@yahoo.co.jp>
2025-05-10 19:46:12 +0900

    Topページ変更(仕掛)

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php include "./asset/html/meta.html"; ?>
-    <link rel="stylesheet" href="./assets/css/top.css" />
+    <link rel="stylesheet" href="./asset/css/top.css" />
     <title>13個のゼリー</title>
 </head>
 
@@ -22,13 +22,13 @@
             <figure><img src="./asset/img/13jellies_B.png"></figure>
             <p>13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans </p>
         </section>
-        <section>
+        <section class="jellies_map">
             <!--
             <script type="module" crossorigin src="https://cf268321.cloudfree.jp/13jellies/13jellies_map/dist/13jellies_map.js"></script>
             <link rel="stylesheet" href="https://cf268321.cloudfree.jp/13jellies/13jellies_map/dist/assets/index.css">
             <div id="root"></div>
             -->
-            <iframe src="https://cf268321.cloudfree.jp/13jellies/13jellies_map/dist/index.html" style="width: 100%; height: 100vh; border: none;"></iframe>
+            <iframe src="https://cf268321.cloudfree.jp/13jellies/13jellies_map/dist/index.html" scrolling="no"></iframe>
         </section>
     </main>
     
