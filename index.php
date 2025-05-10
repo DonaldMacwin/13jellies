da787b0be005e312b4984812ba41aac2231e27a7
Wakabayashi <donald_macwin@yahoo.co.jp>
2025-05-10 19:41:39 +0900

    topページ設営(仕掛)

--- index.php
+++ index.php
@@ -23,7 +23,12 @@
             <p>13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans 13 jelly beans </p>
         </section>
         <section>
-            <figure><img src="./asset/img/13jellies_C.png"></figure>
+            <!--
+            <script type="module" crossorigin src="https://cf268321.cloudfree.jp/13jellies/13jellies_map/dist/13jellies_map.js"></script>
+            <link rel="stylesheet" href="https://cf268321.cloudfree.jp/13jellies/13jellies_map/dist/assets/index.css">
+            <div id="root"></div>
+            -->
+            <iframe src="https://cf268321.cloudfree.jp/13jellies/13jellies_map/dist/index.html" style="width: 100%; height: 100vh; border: none;"></iframe>
         </section>
     </main>
     
